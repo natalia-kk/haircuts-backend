1d46913933713c5f18e7cf604b50717c8e037526
natalia-kk <nataliakloppers@yahoo.com.au>
2021-05-24 16:56:48 +1000

    minor change to home route

--- server.js
+++ server.js
@@ -32,7 +32,7 @@ app.use(fileUpload({ // allows our express server to handle file uploads
 // Ex11b - create home route
 // homepage
 app.get('/', (req, res) => {
-  res.send("Home")
+  res.send("Homepage")
 })
 
 // auth
